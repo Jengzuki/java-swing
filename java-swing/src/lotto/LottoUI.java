08257a84d7a2fd9f88636a8fa09669a0c32e3f3a
HB0201 <HB0201@HB0201-PC>
2015-10-23 17:37:59 +0900

    2015.10.23

--- java-swing/src/lotto/LottoUI.java
+++ java-swing/src/lotto/LottoUI.java
@@ -11,7 +11,7 @@ public class LottoUI extends JFrame implements ActionListener {
 	private static final long serialVersionUID = 1L;
 	Lotto lotto;
 	// 컴포넌트(스윙에서 화면을 나타내는 클래스)는 연관관계로 부모=자식 관계를 맺는다.
-	JButton btn;
+	JButton btn,btnTest,btnExit;
 	JPanel panelNorth, panelSouth; //보더레이아웃 개념으로 볼때 동서남북으로 위치값을 줌.
 	ImageIcon icon;
 	List<JButton> btns;
@@ -26,10 +26,17 @@ public class LottoUI extends JFrame implements ActionListener {
 		btns = new ArrayList<JButton>();
 		panelNorth = new JPanel();
 		panelSouth = new JPanel();
-		btn = new JButton("로또 번호추첨");
+		
+		btn = new JButton("로또번호추첨");
+		btnTest = new JButton("테스트");
+		btnExit = new JButton("종료");
 		/* 조합단계 -> 작은것부터 큰것 순으로 */
 		btn.addActionListener(this);// 이클레스에서 구현한 관련 메소드를 할당한다.
+		btnTest.addActionListener(this);
+		btnExit.addActionListener(this);
 		panelNorth.add(btn);//북쪽 패널에 버튼을 장착
+		panelNorth.add(btnTest); // 북쪽 패널에 버튼장착~
+		panelNorth.add(btnExit);
 		this.add(panelNorth, BorderLayout.NORTH);
 		this.add(panelSouth, BorderLayout.SOUTH);
 		this.setBounds(300, 400, 1200, 300);
@@ -39,19 +46,33 @@ public class LottoUI extends JFrame implements ActionListener {
 	}
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		if (btns.size() == 0) {
-			JButton tmp = null;
-			for (int i = 0; i < 6; i++) {
-				tmp = new JButton(); // 번호가 붙지 않은 로또볼객체를 만들어라.
-				btns.add(tmp); // 6회전이므로 비어있는 로또볼 6새를 만들어 달아라.
-				panelSouth.add(tmp); // 리스트에 담고, 또 그 로또볼을 패널에 붙여라
+		switch (e.getActionCommand()) {
+		case "로또번호추첨":
+			if (btns.size() == 0) {
+				JButton tmp = null;
+				for (int i = 0; i < 6; i++) {
+					tmp = new JButton(); // 번호가 붙지 않은 로또볼객체를 만들어라.
+					btns.add(tmp); // 6회전이므로 비어있는 로또볼 6새를 만들어 달아라.
+					panelSouth.add(tmp); // 리스트에 담고, 또 그 로또볼을 패널에 붙여라
+				}
 			}
+			lotto.setLotto(); // 로또볼 추첨에 들어갑니다.
+			int[] temp = lotto.getLotto();
+			for (int i = 0; i < temp.length; i++) {
+				btns.get(i).setIcon(new ImageIcon("src/image/"+temp[i]+".gif"));
+			}
+			break;
+		case "테스트":
+			System.out.println("까꿍");
+				break;
+				
+		case "종료" :
+			System.exit(0);
+			break;
+			default:
+				break;	
 		}
-		lotto.setLotto(); // 로또볼 추첨에 들어갑니다.
-		int[] temp = lotto.getLotto();
-		for (int i = 0; i < temp.length; i++) {
-			btns.get(i).setIcon(new ImageIcon("src/image/"+temp[i]+".gif"));
-		}
-	}
 
+
+}
 }
